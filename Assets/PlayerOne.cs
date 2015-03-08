f76d43806d07f32497bba77fb00924c8d6b00d0e
Toru <toru.inoue@kissaki.tv>
2015-03-08 21:25:39 +0900

    delete nginx from this repo.

--- Assets/PlayerOne.cs
+++ Assets/PlayerOne.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
 
-class PlayerOne {
+
+
+class PlayerOne : MonoBehaviour {
 	
+	public static USSAWebSocketSharp.WebSocket ws;
+
 	public void Start () {
 		// start websocket connect
+		ws = new USSAWebSocketSharp.WebSocket("ws://127.0.0.1:80/client");
+
+		ws.OnOpen += (sender, e) => {
+			Debug.Log("connected!");
+		};
+
+		ws.OnMessage += (sender, e) => {
+			Debug.Log("message received!");
+		};
+
+		ws.OnError += (sender, e) => {};
+
+		ws.OnClose += (sender, e) => {};
+		
+		ws.Connect();
 	}
 
-	
+
 
 }
