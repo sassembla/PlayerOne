dedba786e26bddb4a633878a290f54e64d971be1
Toru <toru.inoue@kissaki.tv>
2015-03-09 00:38:21 +0900

    add scene

--- Assets/PlayerOne.cs
+++ Assets/PlayerOne.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 
 
 class PlayerOne : MonoBehaviour {
@@ -11,20 +11,21 @@ class PlayerOne : MonoBehaviour {
 		ws = new USSAWebSocketSharp.WebSocket("ws://127.0.0.1:80/client");
 
 		ws.OnOpen += (sender, e) => {
-			Debug.Log("connected!");
+			Debug.Log("connected!:" + e);
 		};
 
 		ws.OnMessage += (sender, e) => {
 			Debug.Log("message received!:" + e.Data);
 		};
 
-		ws.OnError += (sender, e) => {};
+		ws.OnError += (sender, e) => {
+			Debug.Log("disconnected:" + e.Message);
+		};
 
-		ws.OnClose += (sender, e) => {};
+		ws.OnClose += (sender, e) => {
+			Debug.Log("closed:" + e.Reason);
+		};
 		
 		ws.Connect();
 	}
-
-
-
 }
